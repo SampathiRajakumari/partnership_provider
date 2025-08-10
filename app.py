4eb9103e43950738f61e0fce021c949d4a522547
SampathiRajakumari <sampathirajakumari@gmail.com>
2025-08-10 21:23:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -241,6 +241,5 @@ def my_businesses():
 import os
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", debug=True)
 
